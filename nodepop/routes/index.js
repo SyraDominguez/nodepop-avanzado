02aeb6d7d0b47a90b8bd80d9a528d1c30574de20
Syra Dominguez <syradominguez.dev@gmail.com>
2024-05-25 18:43:58 +0200

    fix error

--- nodepop/routes/index.js
+++ nodepop/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const { query, custom, validationResult } = require('express-validator');
-const { routes } = require('../app');
+// const { routes } = require('../app');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
