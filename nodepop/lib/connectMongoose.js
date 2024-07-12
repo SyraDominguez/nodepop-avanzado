d2b48d65c274a68e8282c74315d1821432eea067
Syra Dominguez <syradominguez.dev@gmail.com>
2024-07-12 19:25:19 +0200

    Update MongoDB connection configuration

--- nodepop/lib/connectMongoose.js
+++ nodepop/lib/connectMongoose.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+console.log('DATABASE_URI:', process.env.DATABASE_URI);
+
 const mongoose = require('mongoose');
 
 mongoose.connection.on('error', err => {
@@ -8,6 +11,15 @@ mongoose.connection.once('open', () => {
   console.log('MongoDB connected in', mongoose.connection.name);
 });
 
-mongoose.connect('mongodb://127.0.0.1:27017/nodepop')
+const dbURI = process.env.DATABASE_URI || 'mongodb://127.0.0.1:27017/nodepop';
+
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
 
 module.exports = mongoose.connection;
